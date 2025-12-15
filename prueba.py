867dc9597da9189328e94050df070378c175c697
JOEL VALENCIA <128934402+Valencia525@users.noreply.github.com>
2025-12-15 13:34:28 -0600

    Delete prueba.py

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-
-sd.default.samplerate = 16000
-rec = sd.rec(16000, channels=1)
-sd.wait()
-
-write("prueba.wav", 16000, rec)
-print("Guardado: prueba.wav")
